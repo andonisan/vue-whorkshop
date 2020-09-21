6be1c472c6cd4128c0cfef26e63ac877ef785d32
andonisantamaria <andonisantamaria@hotmail.com>
2020-09-21 16:04:27 +0200

    begin 02-lab

--- workshop/src/components/Heroes.vue
+++ workshop/src/components/Heroes.vue
@@ -1,39 +1,15 @@
 <template>
   <div>
     <h2>Hero</h2>
-    <input v-model="hero.name" type="text" @keyup.enter="upperCaseTheName" />
-    <p>Hello {{ hero.name }}.</p>
-    <p>Your id is {{ hero.id }} and your description is "{{ hero.description }}"</p>
-    <button @click="reverseName">Reverse</button>
   </div>
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { Hero } from '../models/hero';
 
 @Component
-export default class Heroes extends Vue {
-  private hero: Hero;
-
-  constructor() {
-    super();
-    this.hero = {
-      id: 101,
-      name: 'Batman',
-      description: 'el hombre murcielago',
-    };
-  }
-
-  public reverseName(): void {
-    this.hero.name = [...this.hero.name].reverse().join('');
-  }
-
-  private upperCaseTheName(): void {
-    this.hero.name = this.hero.name.toUpperCase();
-  }
-}
+export default class Heroes extends Vue {}
 </script>
 
 <style scoped lang="scss">
